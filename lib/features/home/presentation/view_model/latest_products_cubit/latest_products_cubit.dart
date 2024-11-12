04e8d1d2369f590ec9b8ba3b97100b2d2d8224a2
Ahmed Gamal <ahmedgamal5179@gmail.com>
2024-11-12 19:50:12 +0200

    added stream subscription to latest products cubit to prevent error

--- lib/features/home/presentation/view_model/latest_products_cubit/latest_products_cubit.dart
+++ lib/features/home/presentation/view_model/latest_products_cubit/latest_products_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quick_mart/core/functions/hive_functions.dart';
 import 'package:quick_mart/features/home/data/repos/home_repo.dart';
@@ -7,13 +9,31 @@ class LatestProductsCubit extends Cubit<LatestProductsState> {
   LatestProductsCubit(this.homeRepo) : super(LatestProductsInitial());
 
   HomeRepo homeRepo;
+  StreamSubscription?
+      subscription; //object to listen to the stream and be able to close it when the cubit is closed to prevent emitting new states when cubit is closed
   void getLatestProducts() async {
     emit(LatestProductsLoading());
     String userToken = getLoginToken().toString();
     String lang = getLanguageCode();
-    final result =
-        await homeRepo.getLatestProducts(lang: lang, userToken: userToken);
-    result.fold((fail) => emit(LatestProductsFail(errMsg: fail.errMsg)),
-        (products) => emit(LatestProductsSuccess(products: products)));
+
+    // close any request if there is one
+    await subscription?.cancel();
+
+    subscription = homeRepo
+        .getLatestProducts(lang: lang, userToken: userToken)
+        .asStream()
+        .listen(
+      (result) {
+        result.fold((fail) => emit(LatestProductsFail(errMsg: fail.errMsg)),
+            (products) => emit(LatestProductsSuccess(products: products)));
+      },
+      onError: (e) => emit(LatestProductsFail(errMsg: e.toString())),
+    );
+  }
+
+  @override
+  Future<void> close() {
+    subscription?.cancel();
+    return super.close();
   }
 }
